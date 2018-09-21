ca6ee2a72664b86098a9cd5aacffa35475946744
CreativeGuy2013 <lucacasonato@yahoo.com>
2018-09-21 23:46:35 +0200

    Added basic module register support

--- module/module.go
+++ module/module.go
@@ -7,13 +7,12 @@ type (
 	Module struct {
 		name string
 
-		RegisterTemplateFunction func(identifier string, function func(Request) *Response)
+		templateFunctions map[string]func(Request, *Response)
 	}
 
 	//Request is the request with data and meta from the module caller.
 	Request struct {
-		Error error
-		Data  interface{}
+		Data interface{}
 	}
 
 	//Response is the response to the module API that will be used to respond to the client
@@ -30,3 +29,58 @@ var (
 	//ErrFailed is the error that occurs when the module experiences an internal error.
 	ErrFailed = errors.New("module: internal processing error")
 )
+
+/*
+	Module Management Functions
+*/
+
+//Register registers a new module with its meta information.
+func Register(name string) (module *Module) {
+	return register(name)
+}
+
+func register(name string) *Module {
+	module := new(Module)
+
+	if name == "" {
+		panic("module: name is not defined")
+	}
+
+	module.name = name
+
+	module.templateFunctions = make(map[string]func(Request, *Response))
+
+	return module
+}
+
+/*
+	Module Part Registration Functions
+*/
+
+//TemplateFunctionRegister registers a new template function with identifier "identifier" to the module.
+func (m *Module) TemplateFunctionRegister(identifer string, function func(Request, *Response)) {
+	templateFunctionRegister(m, identifer, function)
+	return
+}
+
+func templateFunctionRegister(m *Module, identifer string, function func(Request, *Response)) {
+	if identifer == "" {
+		panic("module: templateFunctionRegister: identifer is not defined")
+	}
+
+	if m.templateFunctions == nil {
+		panic("module: templateFunctionRegister: initalization of module was not correct")
+	}
+
+	if m.templateFunctions[identifer] != nil {
+		panic("module: templateFunctionRegister: templateFunction with this identifier is already registered")
+	}
+
+	if function == nil {
+		panic("module: templateFunctionRegister: function is not defined")
+	}
+
+	m.templateFunctions[identifer] = function
+
+	return
+}
