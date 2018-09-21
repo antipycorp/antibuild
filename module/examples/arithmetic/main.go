769618f0d94e0021e5564400b433a3e28854fe8c
CreativeGuy2013 <lucacasonato@yahoo.com>
2018-09-22 00:01:17 +0200

    Added example module "arithmetic"

--- module/examples/arithmetic/main.go
+++ module/examples/arithmetic/main.go
@@ -1,25 +1,28 @@
 package main
 
 import (
-	abm "gitlab.com/antipy/antibuild/module/client"
+	"reflect"
+
+	abm "gitlab.com/antipy/antibuild/module"
 )
 
 func main() {
 	module := abm.Register("arithmetic")
 
 	module.TemplateFunctionRegister("add", func(w abm.Request, r *abm.Response) {
-
-		args := w.Data
-
-		// since all data coming in is of type interface and we expect 2 intergers, we have to convert the types
-		a1, ok1 := args[0].(int)
-		a2, ok2 := args[1].(int)
-		if !ok1 || !ok2 {
+		if reflect.TypeOf(w.Data).String() != "[]interface{}" {
 			r.Error = abm.ErrInvalidInput
 			return
 		}
 
-		sum := a1 + a2
+		args := w.Data.([]interface{})
+
+		if reflect.TypeOf(args[0]).String() != "int" || reflect.TypeOf(args[1]).String() != "int" {
+			r.Error = abm.ErrInvalidInput
+			return
+		}
+
+		sum := args[0].(int) + args[1].(int)
 		r.Data = sum
 		return
 	})
