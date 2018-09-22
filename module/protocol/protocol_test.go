7ec3c009d7e89b380f55e10868df6b820e9a3f01
jaap aarts <jaap.aarts1@gmail.com>
2018-09-22 15:03:08 +0200

    \sept-22-2018/ made a protocol.go which can receive and send back data

---
+++ module/protocol/protocol_test.go
@@ -0,0 +1,35 @@
+package protocol
+
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"testing"
+)
+
+func TestProtocol(t *testing.T) {
+	//t.Run("Receive", TestReceive)
+
+	t.Run("Receive", TestReceive)
+}
+
+func TestReceive(t *testing.T) {
+
+	var in bytes.Buffer  // Stand-in for the network.
+	var out bytes.Buffer // Stand-in for the network.
+
+	writer = gob.NewEncoder(&out)
+	reader = gob.NewDecoder(&in)
+
+	var command message
+	var methods getMethods
+	command.Command = "GetAll"
+	command.Payload = methods
+
+	enc := gob.NewEncoder(&in)
+	err := enc.Encode(&command)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(Receive())
+}
