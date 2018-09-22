6404c8c37e317925555682ac734ea15091b0a765
jaap aarts <jaap.aarts1@gmail.com>
2018-09-22 17:27:41 +0200

    \sept-22-2018/ added a AskMethods function, returns methods a function can receive

--- module/protocol/protocol.go
+++ module/protocol/protocol.go
@@ -1,11 +1,16 @@
 package protocol
 
 import (
+	"bytes"
 	"encoding/gob"
+	"errors"
 	"os"
+	"sync"
 )
 
 type (
+	//Methods is a map of available commands to list of functions allowed to be called
+	Methods    map[string][]string
 	getMethods struct{}
 
 	executeMethod struct {
@@ -14,7 +19,7 @@ type (
 	}
 
 	payload interface {
-		excecute([]byte) token
+		excecute([]byte) Token
 	}
 
 	message struct {
@@ -23,61 +28,109 @@ type (
 		ID      []byte
 	}
 
-	token struct {
-		Command string
-		Data    []interface{}
-		ID      []byte
-	}
 	response struct {
 		ID   []byte
 		Data interface{}
 	}
+	//Token is a token used to receive data and send it back to the host
+	Token struct {
+		Command string
+		Data    []interface{}
+		ID      []byte
+	}
 )
 
 var (
-	out    = os.Stdout
-	writer *gob.Encoder
+	outInit sync.Once
+	out     = os.Stdout
+	writer  *gob.Encoder
 
+	inInit sync.Once
 	in     = os.Stdin
 	reader *gob.Decoder
 
-	tokenGetMessages = token{Command: "getmessages"}
+	tokenGetMessages = Token{Command: "getmessages"}
+	tokenReturnVars  = Token{Command: "return vars"}
 )
 
 func init() {
 	gob.RegisterName("message", message{})
 	gob.RegisterName("getmethods", getMethods{})
 
-	writer = gob.NewEncoder(out)
-	reader = gob.NewDecoder(in)
 }
 
-func Receive() token {
+//Receive waits for a command from the host to excecute
+func Receive() Token {
+	inInit.Do(initIn)
 	var command message
 	reader.Decode(&command)
 	return command.excecute()
 }
 
-func (m message) excecute() token {
+func (m message) excecute() Token {
 	return m.Payload.excecute(m.ID)
 }
 
-func (gm getMethods) excecute(id []byte) token {
+func (gm getMethods) excecute(id []byte) Token {
 	ret := tokenGetMessages
 	ret.ID = id
 	return ret
 }
 
-func (gm executeMethod) excecute(id []byte) token {
-	ret := token{Command: gm.Function}
+func (gm executeMethod) excecute(id []byte) Token {
+	ret := Token{Command: gm.Function}
 	ret.ID = id
 	ret.Data = gm.Args
 	return ret
 }
 
-func (t *token) Respond(data interface{}) {
+//Respond sends the given data back to the host
+func (t *Token) Respond(data interface{}) {
 	var resp response
 	resp.Data = data
 	resp.ID = t.ID
 	writer.Encode(resp)
+}
+
+// AskMethods asks for the methods a module can handle, it returns a methods type
+func AskMethods() (Methods, error) {
+	outInit.Do(initOut)
+
+	var command message
+	var m getMethods
+
+	command.Command = "GetAll"
+	command.Payload = m
+
+	writer.Encode(command)
+	resp := waitResponse(command.ID)
+	if resp == nil {
+		if v, ok := resp.(error); ok {
+			return nil, v
+		}
+		return nil, errors.New("could not receive error")
+	}
+	if v, ok := resp.(Methods); ok {
+		return v, nil
+	}
+	return nil, errors.New("return datatype is incorrect")
+}
+
+func waitResponse(id []byte) interface{} {
+	inInit.Do(initIn)
+	var resp response
+	reader.Decode(&resp)
+
+	if !bytes.Equal(resp.ID, id) {
+		return nil
+	}
+	return resp.Data
+}
+
+func initOut() {
+	writer = gob.NewEncoder(out)
+}
+
+func initIn() {
+	reader = gob.NewDecoder(in)
 }
