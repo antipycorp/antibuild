668058968cadb28ed86b3139b7fbb0fab7373014
CreativeGuy2013 <lucacasonato@yahoo.com>
2018-09-23 17:31:01 +0000

    marge new config file into modules

--- module/protocol/protocol.go
+++ module/protocol/protocol.go
@@ -1,17 +1,18 @@
 package protocol
 
 import (
-	"bytes"
 	"encoding/gob"
-	"errors"
+	"io"
 	"os"
 	"sync"
 )
 
 type (
 	//Methods is a map of available commands to list of functions allowed to be called
-	Methods    map[string][]string
-	getMethods struct{}
+	Methods map[string][]string
+
+	//GetMethods is the type used as payload for GetAll
+	GetMethods struct{}
 
 	executeMethod struct {
 		Function string
@@ -19,65 +20,98 @@ type (
 	}
 
 	payload interface {
-		excecute([]byte) Token
+		excecute(ID) Token
 	}
 
 	message struct {
 		Command string
 		Payload payload
-		ID      []byte
+		ID      ID
 	}
 
-	response struct {
-		ID   []byte
+	Response struct {
+		ID   ID
 		Data interface{}
 	}
 	//Token is a token used to receive data and send it back to the host
 	Token struct {
 		Command string
 		Data    []interface{}
-		ID      []byte
+		ID      ID
 	}
+
+	//ID is the type used for identification of the messages
+	ID [10]byte
 )
 
 var (
-	outInit sync.Once
-	out     = os.Stdout
-	writer  *gob.Encoder
+	outInit   sync.Once
+	Out       = io.Writer(os.Stdout)
+	writer    *gob.Encoder
+	writeLock = sync.RWMutex{}
 
-	inInit sync.Once
-	in     = os.Stdin
-	reader *gob.Decoder
+	inInit   sync.Once
+	In       = io.Reader(os.Stdin)
+	reader   *gob.Decoder
+	readLock = sync.RWMutex{}
 
 	tokenGetMessages = Token{Command: "getmessages"}
 	tokenReturnVars  = Token{Command: "return vars"}
+	IDError          = [10]byte{0}
 )
 
 func init() {
 	gob.RegisterName("message", message{})
-	gob.RegisterName("getmethods", getMethods{})
-
+	gob.RegisterName("getmethods", GetMethods{})
 }
 
 //Receive waits for a command from the host to excecute
 func Receive() Token {
-	inInit.Do(initIn)
 	var command message
-	reader.Decode(&command)
+
+	getMessage(&command)
 	return command.excecute()
+}
+
+//GetResponse waits for a response from the client
+func GetResponse() Response {
+	var resp Response
+	getMessage(&resp)
+	return resp
+}
+
+func Send(command string, payload payload, id ID) {
+	outInit.Do(initOut)
+
+	var message message
+
+	message.Command = command
+	message.Payload = payload
+	message.ID = id
+	writeLock.Lock()
+	writer.Encode(command)
+	writeLock.Unlock()
+
+}
+
+func getMessage(message interface{}) {
+	inInit.Do(initIn)
+	readLock.Lock()
+	reader.Decode(message)
+	readLock.Unlock()
 }
 
 func (m message) excecute() Token {
 	return m.Payload.excecute(m.ID)
 }
 
-func (gm getMethods) excecute(id []byte) Token {
+func (gm GetMethods) excecute(id ID) Token {
 	ret := tokenGetMessages
 	ret.ID = id
 	return ret
 }
 
-func (gm executeMethod) excecute(id []byte) Token {
+func (gm executeMethod) excecute(id ID) Token {
 	ret := Token{Command: gm.Function}
 	ret.ID = id
 	ret.Data = gm.Args
@@ -86,51 +120,16 @@ func (gm executeMethod) excecute(id []byte) Token {
 
 //Respond sends the given data back to the host
 func (t *Token) Respond(data interface{}) {
-	var resp response
+	var resp Response
 	resp.Data = data
 	resp.ID = t.ID
 	writer.Encode(resp)
 }
 
-// AskMethods asks for the methods a module can handle, it returns a methods type
-func AskMethods() (Methods, error) {
-	outInit.Do(initOut)
-
-	var command message
-	var m getMethods
-
-	command.Command = "GetAll"
-	command.Payload = m
-
-	writer.Encode(command)
-	resp := waitResponse(command.ID)
-	if resp == nil {
-		if v, ok := resp.(error); ok {
-			return nil, v
-		}
-		return nil, errors.New("could not receive error")
-	}
-	if v, ok := resp.(Methods); ok {
-		return v, nil
-	}
-	return nil, errors.New("return datatype is incorrect")
-}
-
-func waitResponse(id []byte) interface{} {
-	inInit.Do(initIn)
-	var resp response
-	reader.Decode(&resp)
-
-	if !bytes.Equal(resp.ID, id) {
-		return nil
-	}
-	return resp.Data
-}
-
 func initOut() {
-	writer = gob.NewEncoder(out)
+	writer = gob.NewEncoder(Out)
 }
 
 func initIn() {
-	reader = gob.NewDecoder(in)
+	reader = gob.NewDecoder(In)
 }
