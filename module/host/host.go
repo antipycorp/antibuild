95742e67af6c6b28ebe973edb5217e2da2917449
jaap aarts <jaap.aarts1@gmail.com>
2018-09-23 21:49:56 +0200

    \sept-23-2018/ added a version check to the protocol, version is set to 1

--- module/host/host.go
+++ module/host/host.go
@@ -24,6 +24,7 @@ var (
 func Start(in io.Reader, out io.Writer) {
 	protocol.In = in
 	protocol.Out = out
+	protocol.Init(true)
 	go func() {
 		resp := protocol.GetResponse()
 		conn := getCon(resp.ID)
