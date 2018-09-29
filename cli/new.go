29313979a3a25faa68cb6004e98c466be821a942
CreativeGuy2013 <lucacasonato@yahoo.com>
2018-09-29 21:33:21 +0200

    Whohooo!

--- cli/new.go
+++ cli/new.go
@@ -15,9 +15,17 @@
 package cli
 
 import (
+	"archive/zip"
 	"errors"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
 	"regexp"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"gopkg.in/AlecAivazis/survey.v1"
@@ -47,7 +55,7 @@ var newSurvey = []*survey.Question{
 		Name: "template",
 		Prompt: &survey.Select{
 			Message: "Choose a starting template:",
-			Options: []string{"none", "homepage", "newspage"},
+			Options: []string{"basic", "homepage", "newspage"},
 			Default: "homepage",
 		},
 	},
@@ -77,9 +85,124 @@ var newCmd = &cobra.Command{
 			fmt.Println(err.Error())
 			return
 		}
+
+		if _, err := ioutil.ReadDir(answers.Name); os.IsNotExist(err) {
+			dir, err := ioutil.TempDir("", "antibuild")
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			defer os.RemoveAll(dir) // clean up
+
+			downloadFilePath := filepath.Join(dir, "download.zip")
+
+			err = downloadFile(downloadFilePath, "https://build.antipy.com/cli/examples/basic.zip")
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			_, err = unzip(downloadFilePath, answers.Name)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Println("Success. Run these commands to get started:")
+			fmt.Println("")
+			fmt.Println("cd " + answers.Name)
+			fmt.Println("antibuild develop")
+			fmt.Println("")
+			fmt.Println("Need help? Look at our docs: https://build.antipy.com/documentation")
+			fmt.Println("")
+			fmt.Println("")
+			return
+		}
+
+		fmt.Println("Failed.")
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(newCmd)
+}
+
+func unzip(src string, dest string) ([]string, error) {
+	var filenames []string
+
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return filenames, err
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+
+		rc, err := f.Open()
+		if err != nil {
+			return filenames, err
+		}
+		defer rc.Close()
+
+		// Store filename/path for returning and using later on
+		fpath := filepath.Join(dest, f.Name)
+
+		// Check for ZipSlip. More Info: http://bit.ly/2MsjAWE
+		if !strings.HasPrefix(fpath, filepath.Clean(dest)+string(os.PathSeparator)) {
+			return filenames, fmt.Errorf("%s: illegal file path", fpath)
+		}
+
+		filenames = append(filenames, fpath)
+
+		if f.FileInfo().IsDir() {
+
+			// Make Folder
+			os.MkdirAll(fpath, os.ModePerm)
+
+		} else {
+
+			// Make File
+			if err = os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
+				return filenames, err
+			}
+
+			outFile, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+			if err != nil {
+				return filenames, err
+			}
+
+			_, err = io.Copy(outFile, rc)
+
+			// Close the file without defer to close before next iteration of loop
+			outFile.Close()
+
+			if err != nil {
+				return filenames, err
+			}
+
+		}
+	}
+	return filenames, nil
+}
+
+func downloadFile(filepath string, url string) error {
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
