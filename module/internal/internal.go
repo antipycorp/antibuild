418bf09a284c167c7eb86c60d9e251d5d450d953
CreativeGuy2013 <lucacasonato@yahoo.com>
2018-10-05 17:42:21 +0200

    Added internal registerd functions

--- module/internal/internal.go
+++ module/internal/internal.go
@@ -2,11 +2,17 @@ package internal
 
 import (
 	"encoding/gob"
+	"errors"
+	"html/template"
 )
-
-var ()
 
 func init() {
 	gob.Register(map[string]interface{}{})
 	gob.Register([]interface{}{})
+	gob.Register(errors.New("gob"))
+	gob.Register(template.HTML("<h1>GOB</h1>"))
+	gob.Register(template.HTMLAttr("gob"))
+	gob.Register(template.JS("console.log(\"GOB\""))
+	gob.Register(template.JSStr("gob"))
+	gob.Register(template.URL("https://build.antipy.com"))
 }
