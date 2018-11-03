58d3f78e423a92fbe7df3153b2362e0105d40791
jaap aarts <jaap.aarts1@gmail.com>
2018-11-03 17:42:57 +0100

    \nov-03-2018/ added 4 brackets for diferent scopes to make it harder to mass up in the future

--- builder/site/datafile.go
+++ builder/site/datafile.go
@@ -3,7 +3,6 @@ package site
 import (
 	"bytes"
 	"errors"
-	"fmt"
 )
 
 //!!! need to implement post processors !!!
@@ -33,22 +32,24 @@ func (df *datafile) UnmarshalJSON(data []byte) error {
 	loaderData := data[i1+1 : i2]
 	data = data[i2+1:] //data is used for parser
 
-	//get all the arguments for the loader
-	sep := bytes.Split(loaderData, []byte(":"))
-	if len(sep) == 0 {
-		return errors.New("could not get file parser information")
-	}
-	var loader = make([]byte, len(sep[0]))
-	copy(loader, sep[0])
-	//length is not 0 thus everything is fine
-	if len(bytes.Split(sep[0], []byte("_"))) == 1 {
-		loader = append(sep[0], append([]byte("_"), sep[0]...)...)
-	}
-	df.loader = string(loader)
-	df.loaderArguments = ""
+	{
+		//get all the arguments for the loader
+		sep := bytes.Split(loaderData, []byte(":"))
+		if len(sep) == 0 {
+			return errors.New("could not get file parser information")
+		}
+		var loader = make([]byte, len(sep[0]))
+		copy(loader, sep[0])
+		//length is not 0 thus everything is fine
+		if len(bytes.Split(sep[0], []byte("_"))) == 1 {
+			loader = append(sep[0], append([]byte("_"), sep[0]...)...)
+		}
+		df.loader = string(loader)
+		df.loaderArguments = ""
 
-	if len(sep) >= 2 { //only if bigger than 2 this is available
-		df.loaderArguments = string(sep[1])
+		if len(sep) >= 2 { //only if bigger than 2 this is available
+			df.loaderArguments = string(sep[1])
+		}
 	}
 
 	//get the data from for the fileParser
@@ -58,28 +59,28 @@ func (df *datafile) UnmarshalJSON(data []byte) error {
 	parserData := data[i1+1 : i2]
 	data = data[i2+1:] //data should not be empty after this, but might contain leftover data.
 
-	//get all the arguments for the fileParser
-	sep = bytes.Split(parserData, []byte(":"))
-	if len(sep) == 0 {
-		return errors.New("could not get file parser information")
+	{
+		//get all the arguments for the fileParser
+		sep := bytes.Split(parserData, []byte(":"))
+		if len(sep) == 0 {
+			return errors.New("could not get file parser information")
+		}
+
+		var parser = make([]byte, len(sep[0]))
+		copy(parser, sep[0])
+
+		//length is not 0 thus everything is fine
+		if len(bytes.Split(parser, []byte("_"))) == 1 {
+			parser = append(parser, append([]byte("_"), parser...)...)
+		}
+		//fmt.Println(bytes.Split(sep[0], []byte("_")))
+
+		df.parser = string(parser)
+		df.parserArguments = ""
+
+		if len(sep) >= 2 { //only if bigger than 2 this is available
+			df.parserArguments = string(sep[1])
+		}
 	}
-
-	var parser = make([]byte, len(sep[0]))
-	copy(parser, sep[0])
-
-	//length is not 0 thus everything is fine
-	if len(bytes.Split(parser, []byte("_"))) == 1 {
-		parser = append(parser, append([]byte("_"), parser...)...)
-	}
-	//fmt.Println(bytes.Split(sep[0], []byte("_")))
-
-	df.parser = string(parser)
-	df.parserArguments = ""
-
-	if len(sep) >= 2 { //only if bigger than 2 this is available
-		df.parserArguments = string(sep[1])
-	}
-	fmt.Println(df.parser)
-	fmt.Println(df)
 	return nil
 }
