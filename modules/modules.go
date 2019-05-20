4571290cf8448d8b85739acd06e3eabdeb9878f5
Luca Casonato <lucacasonato@yahoo.com>
2019-05-20 12:37:36 +0200

    Moved internal modules to seperate file

--- modules/modules.go
+++ modules/modules.go
@@ -16,15 +16,6 @@ import (
 	"gitlab.com/antipy/antibuild/cli/builder/config"
 	"gitlab.com/antipy/antibuild/cli/builder/site"
 	"gitlab.com/antipy/antibuild/cli/internal/errors"
-
-	abm_file "gitlab.com/antipy/antibuild/std/file/handler"
-	abm_json "gitlab.com/antipy/antibuild/std/json/handler"
-	abm_language "gitlab.com/antipy/antibuild/std/language/handler"
-	abm_markdown "gitlab.com/antipy/antibuild/std/markdown/handler"
-	abm_math "gitlab.com/antipy/antibuild/std/math/handler"
-	abm_noescape "gitlab.com/antipy/antibuild/std/noescape/handler"
-	abm_util "gitlab.com/antipy/antibuild/std/util/handler"
-	abm_yaml "gitlab.com/antipy/antibuild/std/yaml/handler"
 )
 
 type (
@@ -44,50 +35,6 @@ var (
 	sitePostProcessors = &site.SPPs
 
 	iterators = &site.Iterators
-
-	// InternalModules that the are integrated into the antibuild binary
-	InternalModules = map[string]internalMod{
-		"file": internalMod{
-			start:      abm_file.Handler,
-			version:    abm_file.Version,
-			repository: STDRepo,
-		},
-		"json": internalMod{
-			start:      abm_json.Handler,
-			version:    abm_json.Version,
-			repository: STDRepo,
-		},
-		"language": internalMod{
-			start:      abm_language.Handler,
-			version:    abm_language.Version,
-			repository: STDRepo,
-		},
-		"markdown": internalMod{
-			start:      abm_markdown.Handler,
-			version:    abm_markdown.Version,
-			repository: STDRepo,
-		},
-		"math": internalMod{
-			start:      abm_math.Handler,
-			version:    abm_math.Version,
-			repository: STDRepo,
-		},
-		"noescape": internalMod{
-			start:      abm_noescape.Handler,
-			version:    abm_noescape.Version,
-			repository: STDRepo,
-		},
-		"util": internalMod{
-			start:      abm_util.Handler,
-			version:    abm_util.Version,
-			repository: STDRepo,
-		},
-		"yaml": internalMod{
-			start:      abm_yaml.Handler,
-			version:    abm_yaml.Version,
-			repository: STDRepo,
-		},
-	}
 
 	loadedModules = make(map[string]*config.Module)
 
